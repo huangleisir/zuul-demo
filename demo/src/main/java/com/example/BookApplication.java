a101826ba82b4acaf35513f7adaa95734b49d5ef
huanglei <lei.huang@jieshunpay.cn>
2017-12-06 18:16:23 +0800

    checked-out/checked

--- demo/src/main/java/com/example/BookApplication.java
+++ demo/src/main/java/com/example/BookApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class BookApplication {
 
-    @RequestMapping(value = "/available")
+    @RequestMapping(value = "/available/a")
     public String available() {
         return "Spring in Action";
     }
