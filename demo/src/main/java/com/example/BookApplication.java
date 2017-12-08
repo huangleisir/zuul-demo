7f50b09751f9c274a5b399b1382657c2a0995d37
huanglei <lei.huang@jieshunpay.cn>
2017-12-08 09:05:21 +0800

    zuul.prefix=/api true还是false跟这个没关系，他是来设置是否从/rest开始匹配（.path值跟controller路径映射值，从 .path值的哪里开始跟controller路径映射匹配非常关键）  false从/rest开始匹配 默认为true， 从星号开始匹配 所以此时请求/abc zuul.routes.book.stripPrefix=true zuul.routes.book.path=/rest/* zuul.routes.book.url=http://localhost:8090

--- demo/src/main/java/com/example/BookApplication.java
+++ demo/src/main/java/com/example/BookApplication.java
@@ -25,6 +25,27 @@ public class BookApplication {
     public String availableOne() {
         return "Spring in Action";
     }
+//http://localhost:8080/api/rest/abc 
+//将demo2里面的zuul.routes.book.stripPrefix=true toggle切换为false试试看
+    //true表示从星号开始匹配  
+    /**
+zuul.prefix=/api true还是false跟这个没关系，他是来设置是否从/rest开始匹配（.path值跟controller路径映射值，从
+.path值的哪里开始跟controller路径映射匹配非常关键）
+ false从/rest开始匹配 默认为true，
+从星号开始匹配 所以此时请求/abc
+zuul.routes.book.stripPrefix=true 
+zuul.routes.book.path=/rest/*
+zuul.routes.book.url=http://localhost:8090
+     */
+    @RequestMapping(value = "/rest/abc")
+    public String restAbc() {
+        return "restAbc";
+    }
+    
+    @RequestMapping(value = "/abc")
+    public String abc() {
+        return "abc";
+    }
 
     @RequestMapping(value = "/checked-out/checked")
     public String checkedOut() {
