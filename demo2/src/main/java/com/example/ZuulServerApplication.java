52d5010437b99964d70321dd71618365da32bc44
huanglei <lei.huang@jieshunpay.cn>
2017-12-14 16:01:01 +0800

    zuul server 完善

--- demo2/src/main/java/com/example/ZuulServerApplication.java
+++ demo2/src/main/java/com/example/ZuulServerApplication.java
@@ -5,15 +5,62 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableZuulProxy
 @SpringBootApplication
+@RestController
 public class ZuulServerApplication {
 
-    @Bean
+	/*    @Bean
     public SimpleFilter simpleFilter() {
         return new SimpleFilter();
     }
+    @RequestMapping(value = "/api/book2/available/a")
+    public String available() {
+        return "Spring in Action";
+    } 
+    @RequestMapping(value = "/wangyi")
+    public void wangyi() {
+    	System.out.println("转发到网易");
+    }
+    @RequestMapping(value = "/home")
+    public String home() {
+    	return "home";
+    }
+    @RequestMapping(value = "/available")
+    public String availableOne() {
+    	System.out.println(System.currentTimeMillis());
+        return "Spring in Action";
+    }*/
+
+//http://localhost:8080/api/rest/abc 
+//将demo2里面的zuul.routes.book.stripPrefix=true toggle切换为false试试看
+    //true表示从星号开始匹配  
+    /**
+zuul.prefix=/api true还是false跟这个没关系，他是来设置是否从/rest开始匹配（.path值跟controller路径映射值，从
+.path值的哪里开始跟controller路径映射匹配非常关键）
+ false从/rest开始匹配 默认为true，
+从星号开始匹配 所以此时请求/abc
+zuul.routes.book.stripPrefix=true 
+zuul.routes.book.path=/rest/*
+zuul.routes.book.url=http://localhost:8090
+     */
+   /* @RequestMapping(value = "/rest/abc")
+    public String restAbc() {
+        return "restAbc";
+    }
+    
+    @RequestMapping(value = "/abc")
+    public String abc() {
+        return "abc";
+    }
+
+    @RequestMapping(value = "/checked-out/checked")
+    public String checkedOut() {
+        return "Spring Boot in Action";
+    }*/
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulServerApplication.class, args);
@@ -21,3 +68,4 @@ public class ZuulServerApplication {
 
 
 }
+
